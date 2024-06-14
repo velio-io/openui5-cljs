82b53d63953593541a2b7cdf35f478b62e2ea857
Sergey <sergey@minorodata.com>
2024-06-15 01:15:58 +0200

    added public vars

--- src/main/ui5_cljs/core.cljs
+++ src/main/ui5_cljs/core.cljs
@@ -4,7 +4,14 @@
    [ui5-cljs.card :as ui5.card]
    [ui5-cljs.checkbox :as ui5.checkbox]
    [ui5-cljs.input :as ui5.input]
-   [ui5-cljs.select :as ui5.select]))
+   [ui5-cljs.select :as ui5.select]
+   [ui5-cljs.label :as ui5.label]
+   [ui5-cljs.layout :as ui5.layout]
+   [ui5-cljs.line-chart :as ui5.line-chart]
+   [ui5-cljs.message :as ui5.message]
+   [ui5-cljs.modal :as ui5.modal]
+   [ui5-cljs.tab-layout :as ui5.tab-layout]
+   [ui5-cljs.table :as ui5.table]))
 
 
 (def button ui5.button/button)
@@ -14,3 +21,14 @@
 (def input ui5.input/input)
 (def select ui5.select/single-select)
 (def multi-select ui5.select/multi-select)
+(def label ui5.label/label)
+(def layout ui5.layout/layout)
+(def row ui5.layout/row)
+(def column ui5.layout/column)
+(def line-chart ui5.line-chart/line-chart)
+(def message ui5.message/message)
+(def modal ui5.modal/modal)
+(def tab-layout ui5.tab-layout/tab-layout)
+(def tab ui5.tab-layout/tab)
+(def tab-separator ui5.tab-layout/tab-separator)
+(def table ui5.table/table)
